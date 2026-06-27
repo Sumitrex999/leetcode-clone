baeac873f76eb0714d242040da64df9c32dadd87
Sumit Rex <sumitrex666@gmail.com>
2026-06-27 14:57:06 +0530

    Setup Firebase configuration

---
+++ frontend/src/firebase/firebase.ts
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIza........",
+  authDomain: "leetcode-clone-d2d94.firebaseapp.com",
+  projectId: "leetcode-clone-d2d94",
+  storageBucket: "leetcode-clone-d2d94.firebasestorage.app",
+  messagingSenderId: "834603645345",
+  appId: "1:834603645345:web:ba7e6d15caaed03f31b3e0",
+  measurementId: "G-S5LHJHRSQK"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export default app;
